1589b1d5006d6ccde70adacf85600f337007ea69
Evgeny Bunos <evgeny.bunos@wrenkitchens.com>
2020-05-12 19:20:33 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Yauheni Bunas on 5/12/20.
+//  Copyright © 2020 Yauheni Bunas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
